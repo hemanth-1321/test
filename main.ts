a994c67763eca2c97769c6383cb7b50957d84315
hemanth-2113 <hemanth02135@gmail.com>
2025-10-03 12:40:55 +0530

    mod

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+import { mod } from "./helper";
+
 function sum(a: number, c: number) {
   console.log(a + c);
 }
+
+function multiply(a: number, c: number) {
+  console.log(a + c);
+}
+
+const res = mod(9, 8);
+console.log(res);
+
+sum(4, 5);
+multiply(5, 3);
